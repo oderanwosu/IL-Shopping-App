04de162e672e71d7fafebb2e0b3e1c2430ce2bc0
Odera <oderanwosu.j@gmail.com>
2020-12-30 12:52:32 -0800

    Added Saved Shops List View

--- lib/dashboard/home/home.dart
+++ lib/dashboard/home/home.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:iL/styles/constants.dart';
 import 'package:iL/styles/size_helpers.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class Home extends StatefulWidget {
   Home({Key key}) : super(key: key);
@@ -12,7 +14,19 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Column(children: [SizedBox(height: displayHeight(context) * 0.02,),Shops()]),
+      child: Column(children: [
+        SizedBox(
+          height: displayHeight(context) * 0.02,
+        ),
+        Container(
+            alignment: Alignment.topLeft,
+            padding: EdgeInsets.fromLTRB(displayWidth(context) * 0.04, 0, 0, 0),
+            child: Text(
+              'SAVED SHOPS',
+              style: GoogleFonts.oswald(fontSize: displayWidth(context) * 0.03),
+            )),
+        Shops()
+      ]),
     );
   }
 }
@@ -27,41 +41,90 @@ class Shops extends StatefulWidget {
 class _ShopsState extends State<Shops> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-        child: Row(
-      children: [ShopIcon()],
-    ));
+    return Expanded(
+      child: ListView(
+        children: <Widget>[
+          Container(
+            height: 100.0,
+            child: new ListView(
+              scrollDirection: Axis.horizontal,
+              children: [
+                ShopButton(
+                  shopName: 'Nike',
+                  shopAvatar:
+                      'https://data.whicdn.com/images/127461067/original.jpg',
+                ),
+                ShopButton(
+                  shopName: 'Express',
+                  shopAvatar:
+                      'https://finditatthemall.com/system/rich/rich_files/rich_files/000/002/220/original/express1.png?1471638642',
+                ),
+                ShopButton(
+                  shopName: 'Gucci',
+                  shopAvatar:
+                      'https://i.pinimg.com/originals/a5/c4/f3/a5c4f37d260c05806c3566501e302ef0.jpg',
+                ),
+                ShopButton(
+                  shopName: "Macy's",
+                  shopAvatar:
+                      'https://i.pinimg.com/originals/9c/a6/b3/9ca6b375b97cf937d46f5e59408c9c80.jpg',
+                ),
+                ShopButton(
+                  shopName: 'Buyish',
+                  shopAvatar:
+                      'https://images-platform.99static.com//g1UldbxOmPeuSCwiN8Lm_QScXts=/179x179:887x887/fit-in/590x590/99designs-contests-attachments/115/115223/attachment_115223354',
+                ),
+                IconButton(
+                  icon: Icon(Icons.add),
+                  tooltip: 'ADD SHOP',
+                  onPressed: () {
+                    setState(() {});
+                  },
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
 
-class ShopIcon extends StatefulWidget {
-  ShopIcon({Key key}) : super(key: key);
+class ShopButton extends StatefulWidget {
+  final String shopAvatar;
+  final String shopName;
+  ShopButton({this.shopAvatar, this.shopName});
 
   @override
-  _ShopIconState createState() => _ShopIconState();
+  _ShopButtonState createState() => _ShopButtonState();
 }
 
-class _ShopIconState extends State<ShopIcon> {
+class _ShopButtonState extends State<ShopButton> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-        child: new Column(
-      crossAxisAlignment: CrossAxisAlignment.center,
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: <Widget>[
-        new Container(
-            width: displayHeight(context) * 0.08,
-            height: displayHeight(context) * 0.08,
-            decoration: new BoxDecoration(
-                border: Border.all(width: displayWidth(context) * 0.01, color: Colors.grey ),
-                
-                shape: BoxShape.circle,
-                image: new DecorationImage(
-                    fit: BoxFit.fill,
-                    image:
-                        new NetworkImage("https://i.imgur.com/BoN9kdC.png")))),
-        new Text("John Doe", textScaleFactor: 1.5)
-      ],
-    ));
+    return FlatButton(
+        onPressed: null,
+        child: Container(
+            child: new Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+              new Container(
+                  width: displayHeight(context) * 0.071,
+                  height: displayHeight(context) * 0.071,
+                  decoration: new BoxDecoration(
+                      // border: Border.all(
+                      //     width: displayWidth(context) * 0.09,
+                      //     color: kDeactiveColor),
+                      shape: BoxShape.circle,
+                      image: new DecorationImage(
+                          fit: BoxFit.fitWidth,
+                          image: new NetworkImage(widget.shopAvatar)))),
+              new Text(widget.shopName,
+                  style: GoogleFonts.quicksand(
+                      color: Colors.black,
+                      fontWeight: FontWeight.w700,
+                      fontSize: displayHeight(context) * 0.012))
+            ])));
   }
 }
--- lib/styles/constants.dart
+++ lib/styles/constants.dart
@@ -3,3 +3,4 @@ import 'package:flutter/material.dart';
 const kPrimaryColor = Color.fromRGBO(43, 46, 57, 1.0);
 const kSecondary = Colors.white;
 const kTopBarBlack = Color.fromRGBO(32, 34, 43, 1.0);
+const kDeactiveColor = Color.fromRGBO(235, 235, 235, 1.0);
